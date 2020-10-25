d14fa7eebe1325bf0f954026b452dc1e92245b08
Pann Ei Phyu <panneiphyu5cs43@gmail.com>
2020-10-25 15:11:41 +0630

    first commit

---
+++ about.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">  
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <title>HSP Digital Marketing Agency</title>
+    <link rel="stylesheet" href="library/bootstrap/bootstrap.min.css">
+    <link rel="stylesheet" href="library/fontawesome/fontawesome-all.min.css">
+    <link rel="stylesheet" href="css/style.css">
+      <link rel="shortcut icon" href="images/Logo-Website/32x32 px - Logo.jpg">
+      <style>
+        .text-red{ 
+                  color: #8D4242;
+                }
+          h4 {
+          width: 250px;
+          height: 60px;
+          line-height: 60px;
+          text-align: center;
+          border-radius: 20px 20px 20px 20px;
+
+         }    
+         h4.bg-brown{
+          background-color: #8D4242;
+         }     
+
+   
+    </style>
+</head>
+<body>
+
+
+        <div class="container">
+          <div class="row mt-5">
+            <div class="col-md-4">
+              <a href="index.php">
+              <img src="images/Logo-Website/Original Logo-png.png" alt="" width="100px;" class="mr-auto mb-2">
+              </a>
+
+            </div>
+            <div class="col-md-8 mr-auto"> 
+            <h4 class="text-center text-white bg-brown">
+              ကျွန်ုပ်တို့အကြောင်း
+              </h4>             
+            </div>
+          </div>
+        </div>
+
+        
+
+  <section id="#about" class="my-5 px-3">
+    <div class="container">         
+          <div class="row my-3 mx-0" style="background-color: #cce6ff;">
+            <div class="col-sm-5 pl-0 mt-3">
+              <img src="images/Clients/HSP/About - HSP/slide.png" alt="" class="img-fluid ml-5" width="400px;" height="200px;">
+            </div>
+            <div class="col-sm-7 mt-5" >
+              <h5 class="text-red"><b>Facebook Marketing</b></h5>
+              <h5 class="text-red py-4">
+                ကျွန်မတို့၏ ဆန်းသစ်သော Idea များ၊ Digital Marketing နှင့် ပတ်သက်၍ ရရှိထားသော အတွေ့အကြုံများဖြင့် လူကြီးမင်းတို့၏ Product တွေ၊ Brand တွေကို Customer များ ဝယ်ယူအသုံးပြုလာရန် ဖန်တီးပေးပြီး ပုံမှန် Facebook Follower များမှတစ်ဆင့် လူကြီးမင်းတို့၏ Product များကို အမှန်တကယ်သုံးစွဲသော Customer များဖြစ်လာစေရန်ကူညီဆောင်ရွက်ပေးနေပါသည်။ 
+              </h5>
+            </div>
+          </div>
+    </div>
+  </section>
+
+  <section id="#about" class="my-5 px-3">
+    <div class="container">
+          <div class="row my-3 mx-0" style="background-color: #B6F474;">
+            <div class="col-sm-5 pl-0 mt-3">
+              <img src="images/Clients/HSP/About - HSP/display.jpg" alt="" class="img-fluid ml-5" width="400px;" height="200px;">
+            </div>
+            <div class="col-sm-7 mt-5">
+              <h5 class="text-red"><b>Messenger Marketing</b></h5> 
+              <h5 class="text-red py-4">
+                လူကြီးမင်းတို့ Page ရဲ့ Messenger မှာ Chatbot တည်ဆောက်ခြင်းဖြင့် လူကြီးမင်းတို့၏ လုပ်ငန်း အလျင်အမြန်တိုးတက်စေရန်အတွက် Message Box မှ အလိုအလျောက်ဖြေကြားမှုများ၊ Customer မေးသော အခြားသော မေးခွန်းများအား ဖြေဆိုပေးခြင်း၊ လုပ်ငန်းအမျိုးအစားပေါ်မူတည်ပြီး Chatbot တည်ဆောက်ပေးခြင်းတို့ ပြုလုပ်ပေးသွားမှာ ဖြစ်ပါတယ်....
+              </h5>
+            </div>
+          </div>
+    </div>
+  </section>
+  
+
+    <script src="library/bootstrap/jquery-3.4.1.slim.min.js"></script>
+    <script src="library/bootstrap/popper.min.js"></script>
+    <script src="library/bootstrap/bootstrap.min.js"></script>
+    <script src="js/script.js"></script>
+</body>
+</html>
